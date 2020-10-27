4ae2fb4b6fcd2abe73a21019c2c520192ddb259b
Sai Purciful <saipurciful@gmail.com>
2020-10-27 15:12:42 -0700

    added more info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+// for typewriting when click
 var i = 0;
-var txt = 'Hi I am Sai....Web Developer'; /* The text */
-var speed = 250; /* The speed/duration of the effect in milliseconds */
+var txt = 'Hi I am Sai....Web Developer'; 
+var speed = 250; 
 
 function typeWriter() {
   if (i < txt.length) {
@@ -18,10 +18,8 @@ window.onscroll = function() {myFunction()};
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -29,3 +27,5 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
